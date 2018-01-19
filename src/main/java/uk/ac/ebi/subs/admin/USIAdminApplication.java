67b12e855213e9f6f0d9158d8724bd29aad95c32
Neil Goodgame <neilg@ebi.ac.uk>
2018-01-19 11:11:35 +0000

    Initial commit for basic admin server

---
+++ src/main/java/uk/ac/ebi/subs/admin/USIAdminApplication.java
@@ -0,0 +1,18 @@
+package uk.ac.ebi.subs.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
+
+@EnableAdminServer
+@SpringBootApplication
+    public class USIAdminApplication {
+ 
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication( USIAdminApplication.class);
+        ApplicationPidFileWriter applicationPidFileWriter = new ApplicationPidFileWriter();
+        springApplication.addListeners( applicationPidFileWriter );
+        springApplication.run(args);
+    }
+}
