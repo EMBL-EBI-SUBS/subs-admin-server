b52098554a0439341a49ac6e94fccfd0795d41d7
Karoly Erdos <karoly@ebi.ac.uk>
2019-12-10 17:49:45 +0000

    Upgrade to Spring Boot Admin 2.1.6
    Changes:
    - Update to Spring Boot 2.1.9
    - Update to OpenJDK 11 and Gradle 6.01
    - Update Travis config using Java 11

--- src/main/java/uk/ac/ebi/subs/admin/USIAdminApplication.java
+++ src/main/java/uk/ac/ebi/subs/admin/USIAdminApplication.java
@@ -1,14 +1,24 @@
 package uk.ac.ebi.subs.admin;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.system.ApplicationPidFileWriter;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
-@EnableAdminServer
+import java.util.UUID;
+
 @SpringBootApplication
+@EnableAdminServer
 public class USIAdminApplication {
  
     public static void main(String[] args) {
@@ -16,5 +26,49 @@ public class USIAdminApplication {
         ApplicationPidFileWriter applicationPidFileWriter = new ApplicationPidFileWriter();
         springApplication.addListeners( applicationPidFileWriter );
         springApplication.run(args);
+    }
+
+    @Configuration
+    @EnableWebSecurity
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        private final AdminServerProperties adminServer;
+
+        public WebSecurityConfig(AdminServerProperties adminServer) {
+            this.adminServer = adminServer;
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+            successHandler.setTargetUrlParameter("redirectTo");
+            successHandler.setDefaultTargetUrl(this.adminServer.getContextPath() + "/");
+
+            http.authorizeRequests()
+                    .antMatchers(this.adminServer.getContextPath() + "/assets/**")
+                    .permitAll()
+                    .antMatchers(this.adminServer.getContextPath() + "/login")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .formLogin()
+                    .loginPage(this.adminServer.getContextPath() + "/login")
+                    .successHandler(successHandler)
+                    .and()
+                    .logout()
+                    .logoutUrl(this.adminServer.getContextPath() + "/logout")
+                    .and()
+                    .httpBasic()
+                    .and()
+                    .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .ignoringRequestMatchers(new AntPathRequestMatcher(this.adminServer.getContextPath() + "/instances", HttpMethod.POST.toString()), new AntPathRequestMatcher(this.adminServer.getContextPath() + "/instances/*", HttpMethod.DELETE.toString()),
+                            new AntPathRequestMatcher(this.adminServer.getContextPath() + "/actuator/**"))
+                    .and()
+                    .rememberMe()
+                    .key(UUID.randomUUID()
+                            .toString())
+                    .tokenValiditySeconds(1209600);
+        }
     }
 }
